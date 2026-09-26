6d04a422b445aed0b7b3dfe70b238e24e880ffe6
Adam Burdett <burdettadam@users.noreply.github.com>
2026-09-26 11:17:22 -0600

    test: include passport callback signer in Rust image allowlist

--- tests/test_shared_rust_service_image.py
+++ tests/test_shared_rust_service_image.py
@@ -21,6 +21,7 @@ RUST_SERVICES = {
     "issuance_native": "marty-issuance-service",
     "notification": "marty-notification",
     "organization": "marty-organization",
+    "passport_callback_signer": "marty-passport-callback-signer",
     "presentation_policy": "marty-presentation-policy",
     "revocation_profile": "marty-revocation-profile",
     "signing_keys": "marty-signing-keys",
